3a7e99cb82baa4e50dd1c506094d1700373abdab
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-09-18 11:24:40 -0600

    Agregar ayudante de token de autenticación
    - Agrega un nuevo método estático, ObtenerToken(), a la clase AuthHelper.
    - Este método simplifica la recuperación del token de autenticación a partir de los reclamos de un usuario, mejorando la legibilidad del código y la reutilización en toda la aplicación.

---
+++ FrontAuth.WebApi/Helpers/AuthHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace FrontAuth.WebApi.Helpers
+{
+    // Clase de utilidad para asistir en la gestión de la autenticación.
+    public class AuthHelper
+    {
+        // Extrae el token de autenticación de los claims del usuario.
+        public static string ObtenerToken(ClaimsPrincipal user)
+        {
+            return user.FindFirstValue("Token")?? string.Empty;
+        }
+    }
+}
