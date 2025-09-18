0323bd6e9fb35e3643b86cab908d732d7f2fe53c
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-09-17 21:18:49 -0600

    Diseñar AuthService
    -Se implemento AuthServiceo
    -Implementa métodos de inicio y registro de usuarios

---
+++ FrontAuth.WebApi/Services/AuthService.cs
@@ -0,0 +1,25 @@
+﻿using FrontAuth.WebApi.DTOs.UsuarioDTOs;
+
+namespace FrontAuth.WebApi.Services
+{
+    public class AuthService
+    {
+        // Declara una dependencia para realizar llamadas a la API.
+        private readonly ApiService _apiService;
+
+        // Inyecta el servicio de API a través del constructor.
+        public AuthService(ApiService apiService) { _apiService = apiService; }
+
+        // Método para manejar el inicio de sesión de un usuario.
+        public async Task<LoginResponseDTO> LoginAsync(UsuarioLoginDTO dto)
+        {
+            return await _apiService.PostAsync<UsuarioLoginDTO, LoginResponseDTO>("auth/login", dto);
+        }
+
+        // Método para manejar el registro de un nuevo usuario.
+        public async Task<LoginResponseDTO> RegistrarAsync(UsuarioRegistroDTO dto)
+        {
+            return await _apiService.PostAsync<UsuarioRegistroDTO, LoginResponseDTO>("auth/registrar", dto);
+        }
+    }
+}
