ccc405d2053c965012bda20d214564e261df3c6e
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-09-17 21:43:45 -0600

    Agregar carpeta Helpers
    - Implementación de la gestión de Claims para la identidad de usuario
    - Introduce una nueva clase de ayudante estática para manejar la creación de objetos ClaimsPrincipal a partir de datos de autenticación de usuario.
    - Esto centraliza la lógica para mapear detalles del usuario como nombre, correo electrónico, rol y un token de seguridad en reclamaciones, que son esenciales para la autenticación y autorización.

---
+++ FrontAuth.WebApi/Helpers/ClaimsHelper.cs
@@ -0,0 +1,26 @@
+﻿using FrontAuth.WebApi.DTOs.UsuarioDTOs;
+using System.Security.Claims;
+
+namespace FrontAuth.WebApi.Helpers
+{
+    // Define una clase de ayuda estática para gestionar claims.
+    public static class ClaimsHelper
+    {
+        // Método para construir una identidad de usuario a partir de sus datos de inicio de sesión.
+        public static ClaimsPrincipal CrearClaimsPrincipal(LoginResponseDTO usuario)
+        {
+            // Se crea una lista de declaraciones (claims) del usuario.
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name, usuario.Nombre),
+                new Claim(ClaimTypes.Email,usuario.Email),
+                new Claim(ClaimTypes.Role, usuario.Rol),
+                new Claim("Token", usuario.Token)
+            };
+            // Crea una identidad de claims con las declaraciones y el esquema de autenticación.
+            var identity = new ClaimsIdentity(claims, "AuthCookie");
+
+            // Devuelve un objeto ClaimsPrincipal, que es el contenedor de la identidad.
+            return new ClaimsPrincipal(identity);
+        }
+    }
+}
