678e106e9527860211527c9e34f6965044314546
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-09-18 11:38:32 -0600

    Agregar vista de usuarios con autenticación
    - Implementa la acción 'Index' en el controlador UsuarioController.
    - Utiliza el método AuthHelper.ObtenerToken() para asegurar que la solicitud a la API se autentique correctamente con un token JWT antes de recuperar la lista de usuarios.

---
+++ FrontAuth.WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using FrontAuth.WebApi.DTOs.UsuarioDTOs;
+using FrontAuth.WebApi.Helpers;
+using FrontAuth.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontAuth.WebApi.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ApiService _apiService;
+        public UsuarioController(ApiService apiService) { _apiService = apiService; }
+
+        //Get: Index
+        // Muestra la vista de índice con una lista de usuarios.
+        public async Task<IActionResult> Index()
+        {
+            // Obtiene el token de autenticación del usuario actual utilizando la clase de ayuda.
+            var token = AuthHelper.ObtenerToken(User); // Obtener el token JWT del usuario autenticado desde los claims
+
+            var usuarios = await _apiService.GetAllAsync<UsuarioDTO>("User/usuarios", token);
+            return View(usuarios);
+        }
+    }
+}
